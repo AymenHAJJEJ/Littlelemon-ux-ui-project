22011a6b334b0951cace5b1b138aed86fe49d7ef
Aymen Hajjej <112734141+AymenHAJJEJ@users.noreply.github.com>
2025-08-13 19:00:44 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace Classes
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+RouteGroupBuilder todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
+todoItems.MapPost("/", CreateTodo);
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo);
+
+app.Run();
+
+static async Task<IResult> GetAllTodos(TodoDb db)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //var customer = new Customer(1);
-            //customer.Orders.Add(new Order());
-            //customer.Orders.Add(new Order());
-            //Console.WriteLine(customer.Orders.Count);
-            //try
-            //{
-            //    var num = int.Parse("abc");
-            //}
-            //catch (Exception) 
-            //{
-            //    Console.WriteLine("Conversion failed");
-            //}
-            //var number;
-            //var result = int.TryParse("abc", out number);
-            //if (result)
-            //    Console.WriteLine(number);
-            //else
-            //    Console.WriteLine("Conversion failed");
-        }
-        static void UseParams()
-        {
-            var calculator = new Calculator();
-            Console.WriteLine(calculator.Add(1, 2));
-            Console.WriteLine(calculator.Add(1, 2, 3));
-            Console.WriteLine(calculator.Add(1, 2, 3, 4));
-            Console.WriteLine(calculator.Add(1, 2, 3, 4, 5));
-        }
-        static void UsePoints()
-        {
-            try
-            {
-                var point = new Point(10, 20);
-                point.Move(new Point(40, 60));
-                Console.WriteLine("Point is at: {0}, {1}", point.X, point.Y);
+    return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+}
 
-                point.Move(100, 200);
-                Console.WriteLine("Point is at: {0}, {1}", point.X, point.Y);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("An expected error occured");
-            }
-        }
+static async Task<IResult> GetCompleteTodos(TodoDb db) {
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
+}
+
+static async Task<IResult> GetTodo(int id, TodoDb db)
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+            ? TypedResults.Ok(new TodoItemDTO(todo))
+            : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
+{
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name
+    };
+
+    db.Todos.Add(todoItem);
+    await db.SaveChangesAsync();
+
+    todoItemDTO = new TodoItemDTO(todoItem);
+
+    return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
+}
+
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return TypedResults.NotFound();
+
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> DeleteTodo(int id, TodoDb db)
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
     }
+
+    return TypedResults.NotFound();
 }
